d1da55cd15f9498057a50f7bd1a13e2f18ea84f3
sofia1098 <sofianunez000@gmail.com>
2025-08-01 16:31:36 +0000

    dos sin terminar

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+
+/**
+ *
+ * binary_tree_insert_right - 
+ * @parent:
+ * @value:
+ *
+ * Return: 
+ */
+
+binary_tree_t *binary_tree_insert_right(binary)
+{
+}
