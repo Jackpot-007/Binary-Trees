9134378e0ce2132b4d626e298d6c6076c352dd74
sofia1098 <sofianunez000@gmail.com>
2025-08-02 15:22:51 +0000

    dos y tres

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -2,14 +2,36 @@
 #include <stdlib.h>
 
 /**
+ * binary_tree_insert_right - Crea un nuevo nodo a la derecha
+ * @parent: Puntero al nodo padre del nodo ha crear
+ * @value: valor del nuevo nodo
  *
- * binary_tree_insert_right - 
- * @parent:
- * @value:
- *
- * Return: 
+ * Return: puntero al nuevo nodo
  */
 
-binary_tree_t *binary_tree_insert_right(binary)
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
+	binary_tree_t *nodo;
+
+	if (parent == NULL)
+		return (NULL);
+
+	nodo = malloc(sizeof(binary_tree_t));
+	if (nodo == NULL)
+		return (NULL);
+
+	/*declaracion de todos los elementos del nodo creado*/
+	nodo->n = value;
+	nodo->parent = parent;
+	nodo->left = NULL;
+	nodo->right = NULL;
+
+	if (parent->right != NULL)
+	{
+		nodo->right = parent->right;
+		parent->right = nodo;
+	}
+
+	parent->right = nodo;
+	return (nodo);
 }
