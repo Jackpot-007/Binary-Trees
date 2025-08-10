ee92e7ec540c362077dc471940330309320eaed7
Jackpot-007 <jackpotmerlon@gmail.com>
2025-08-10 00:15:46 +0000

    arreglo 4.1 beta

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -12,35 +12,27 @@
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	if (tree == NULL)
+	if (tree)
 		return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
 
 	return (0);
 }
 
 /**
- * binary_tree_height - Mide la altura del arbol
- * @tree: Puntero al nodo raiz
+ * binary_tree_height - Measures the height of a binary tree.
+ * @tree: A pointer to the root node of the tree to measure the height.
  *
- * Return: Altura, o 0 si el arbol es NULL
+ * Return: If tree is NULL, your function must return 0, else return height.
  */
-
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t altura_izq = 0;
-	size_t altura_der = 0;
-	/*Caso base*/
+	if (tree)
+	{
+		size_t l = 0, r = 0;
 
-	if (tree == NULL)
-		return (0);
-	/*si tiene hijo izq recorro y sumo 1 por cada interaccion*/
-	if (tree->left != NULL)
-		altura_izq = 1 + binary_tree_height(tree->left);
-	if (tree->right != NULL)
-		altura_der = 1 + binary_tree_height(tree->right);
-	/*reviso que altura es mas alta*/
-	if (altura_izq > altura_der)
-		return (altura_izq);
-
-	return (altura_der);
+		l = tree->left ? 1 + binary_tree_height(tree->left) : 1;
+		r = tree->right ? 1 + binary_tree_height(tree->right) : 1;
+		return ((l > r) ? l : r);
+	}
+	return (0);
 }
