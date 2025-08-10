6766e769916003f803f7b3e66bae128c786b631d
Jackpot-007 <jackpotmerlon@gmail.com>
2025-08-10 01:27:59 +0000

    arreglo 7.0

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -26,7 +26,7 @@ int binary_tree_balance(const binary_tree_t *tree)
  * the tree to measure the height.
  *
  * Return: If tree is NULL, your function
- * must return 0, else return height.
+ * must return 0, else return height
  */
 
 size_t binary_tree_height(const binary_tree_t *tree)
